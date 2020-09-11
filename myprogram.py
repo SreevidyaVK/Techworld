1cd878fbbab2ffac72a75a5a86da8338b11d2326
SreevidyaVK <sreevidyasanoj@gmail.com>
2020-09-11 14:36:00 +0530

    first commit

---
+++ myprogram.py
@@ -0,0 +1,15 @@
+import json
+import os
+from miner_text_generator import extract_text_by_page
+def export_as_json(pdf_path, json_path):
+    filename=os.path.splitext(os.path.basename(pdf_path))[0]
+    data={}
+    for line in extract_text_by_page(pdf_path):
+            key,value=line.strip().split(None, 1)
+            data[key]=value.strip()
+    out_file = open(json_path,'w')
+    json.dump(data, out_file)
+if __name__ == '__main__':
+    pdf_path = 'Interview_sample_data.pdf'
+    json_path = 'sampledata.json'
+    export_as_json(pdf_path, json_path)
