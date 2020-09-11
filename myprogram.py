7131d08a8367c65bcfda3b70f7eea042f90496b6
SreevidyaVK <sreevidyasanoj@gmail.com>
2020-09-11 14:53:32 +0530

    first commit

--- myprogram.py
+++ myprogram.py
@@ -5,11 +5,11 @@ def export_as_json(pdf_path, json_path):
     filename=os.path.splitext(os.path.basename(pdf_path))[0]
     data={}
     for line in extract_text_by_page(pdf_path):
-            key,value=line.strip().split(None, 1)
-            data[key]=value.strip()
+            command,description=line.strip().split(None, 1)
+            data[command]=description.strip()
     out_file = open(json_path,'w')
     json.dump(data, out_file)
 if __name__ == '__main__':
     pdf_path = 'Interview_sample_data.pdf'
-    json_path = 'sampledata.json'
+    json_path = 'sample_data.json'
     export_as_json(pdf_path, json_path)
